bde7c4215d899eeda763d7cb63f080ba9015c37c
Richard Michael <rcml@novozymes.com>
2020-12-20 15:36:36 +0100

    fixed MCMC model conditioning, tensor inpu

--- data_scaler.py
+++ data_scaler.py
@@ -1,8 +1,9 @@
+import numpy as np
 import torch
 import pyro
 from pyro.infer import MCMC, NUTS
-import pyro.poutine as poutine
-import pyro.distributions as d
+from pyro.infer.mcmc.util import initialize_model
+import pyro.distributions as dist
 
 class BayesScaler:
     """
@@ -14,7 +15,7 @@ class BayesScaler:
         self.samples_N = 10000
         self.warmup_N = 500
         self.chains_N = 1
-        self.y = ΔΔg
+        self.y = torch.Tensor(ΔΔg)
         self.α_a, self.β_a = α_b, β_b
         self.α_b, self.β_b = α_a, β_a
         self.α_c, self.β_c = α_c, β_c
@@ -24,26 +25,24 @@ class BayesScaler:
         self.y = self.mcmc.get_samples()['obs'].mean(0)
         self.σ_T = self.y - 0 # TODO difference between posterior mean and individual value ???
 
-    def _model(self):
-        a = pyro.sample('a', d.Gamma(self.α_a, self.β_a))
-        b = 0.5 * pyro.sample('b', d.Beta(self.α_b, self.β_b))
-        c = (10/3) * pyro.sample('c', d.Beta(self.α_c, self.β_c))
-        d = pyro.sample('d', d.Normal(-a, self.σ_d))
+    def _model(self, y):
+        n_obs = y.shape[0]
+        with pyro.plate("n_obs", n_obs):
+            a = pyro.sample('a', dist.Gamma(self.α_a, self.β_a))
+            b = 0.5 * pyro.sample('b', dist.Beta(self.α_b, self.β_b))
+            c = (10/3) * pyro.sample('c', dist.Beta(self.α_c, self.β_c))
+            d = pyro.sample('d', dist.Normal(-a, self.σ_d))
+            θ = a * torch.exp(c*y) + b*y + d
 
-        θ = a * torch.exp(c*self.y) + b*self.y + d
-
-        return pyro.sample("obs", d.Normal(θ, self.σ_n))
-
-    def _conditioned_model(self):
-        """
-        treat observations as ocnditionally independent
-        """
-        poutine.condition(self._model, data={'obs': self.y})(self.σ_n)
+            return pyro.sample("obs", dist.Normal(θ, self.σ_n), obs=y)
     
     def run_mcmc(self):
-        nuts_kernel = NUTS(self._conditioned_model, jit_compile=True)
-        mcmc = MCMC(nuts_kernel, num_samples=self.samples_N, warmup_steps=self.warmup_N, num_chains=self.chains_N)
-        mcmc.run(self._model)
+        # init_params, fn, transforms, _ = initialize_model(self._model, model_args=(self.y))
+        # nuts_kernel = NUTS(potential_fn=fn, jit_compile=True)
+        # mcmc = MCMC(nuts_kernel, initial_parameters=init_params, num_samples=self.samples_N, warmup_steps=self.warmup_N, num_chains=self.chains_N)
+        nuts_kernel = NUTS(self._model)
+        mcmc = MCMC(nuts_kernel, num_samples=self.samples_N, warmup_steps=self.warmup_N)
+        mcmc.run(self.y)
         return mcmc
        
 
