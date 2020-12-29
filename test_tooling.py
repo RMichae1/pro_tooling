839258732e71e279574fa90b10826371c9149bbe
Richard Michael <rcml@novozymes.com>
2020-12-29 21:45:32 +0100

    test additive noise model and GPR

--- test_tooling.py
+++ test_tooling.py
@@ -1,8 +1,10 @@
 from contact_mapper import ContactMapper
 from protein_representation import ProteinCollection, ProteinCollectionSimulated
+from protein_representation import AdditiveNoiseRepresentation
 from graphkernel import MatrixKernel
 from data_handler import parse_mutations
 from data_scaler import BayesScaler
+from gp_regression import GPRegression
 
 if __name__ == "__main__":
 
@@ -52,22 +54,30 @@ if __name__ == "__main__":
     print("Computed Kernel Value - WT w/ BLOSUM 62 {}".format(wdk.kernel_value))
     #print(wdk.K_ϕ)
 
-    # mutational_dict = parse_mutations("./data/ddg_protherm.mat", query="ddg_protherm")
-    # pcol = ProteinCollection(cm_tri, pdb_ID="1PGA", pdb_mutations=mutational_dict)
-    # print(pcol.matrices_df)
+    mutational_dict = parse_mutations("./data/ddg_protherm.mat", query="ddg_protherm")
+    pcol = ProteinCollection(cm_tri, pdb_ID="1PGA", pdb_mutations=mutational_dict)
+    print(pcol.matrices_df)
     # pcol.plot_sub_matrices()
-    # print(pcol.mwdk_df)
+    print(pcol.mwdk_df)
     # pcol.plot_mwdk()
 
-    ros_mut_dict = parse_mutations("./data/ddg_rosetta_single.mat", query="ddg_rosetta_single")
+    # ros_mut_dict = parse_mutations("./data/ddg_rosetta_single.mat", query="ddg_rosetta_single")
 
-    # instantiate and parse IS data
-    rosetta_collection = ProteinCollection(cm_tri, pdb_ID="1PGA", pdb_mutations=ros_mut_dict)
-    # scale using Bayesian Scaling
-    bs_rosetta = BayesScaler(ΔΔg=rosetta_collection.ΔΔg)
-    print(bs_rosetta.y)
-    print(bs_rosetta.ΔΔg)
-    print(bs_rosetta.σ_T)
-    print(bs_rosetta.plot_scaling())
-    #rosetta_collection = ProteinCollectionSimulated(cm_tri, pdb_ID="1PGA", pdb_mutations=ros_mut_dict)
-    #print(rosetta_collection.ΔΔg)
+    # # instantiate and parse IS data
+    # rosetta_collection = ProteinCollection(cm_tri, pdb_ID="1PGA", pdb_mutations=ros_mut_dict)
+    # # scale using Bayesian Scaling
+    # bs_rosetta = BayesScaler(ΔΔg=rosetta_collection.ΔΔg)
+    # print(bs_rosetta.θ)
+    # print(bs_rosetta.ΔΔg)
+    # #print(bs_rosetta.mcmc_samples)
+    # print(bs_rosetta.print_summary())
+    # print(bs_rosetta.σ_T)
+    # print(len(bs_rosetta.σ_T))
+    # #print(bs_rosetta.plot_scaling())
+    # #rosetta_collection = ProteinCollectionSimulated(cm_tri, pdb_ID="1PGA", pdb_mutations=ros_mut_dict)
+    # #print(rosetta_collection.ΔΔg)
+
+    noisy_protein = AdditiveNoiseRepresentation(protein_representation=pcol)
+
+    gpr = GPRegression(protein_representation=pcol, noise_factor=noisy_protein)
+    print(gpr.p_sample)
