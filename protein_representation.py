8dab0696e0837e4811a0e49525a0995e38ff123f
Richard Michael <rcml@novozymes.com>
2021-01-06 15:21:08 +0100

    combined experimental and simulated data in protein representation

--- protein_representation.py
+++ protein_representation.py
@@ -10,23 +10,36 @@ from graphkernel import KernelFactory
 from data_scaler import BayesScaler
 from typing import Tuple
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 import torch
 from torch.distributions import Normal, Gamma
 from torch.nn import Parameter
+from sklearn.preprocessing import StandardScaler ## TODO take out this quickfix
 
+scaler = StandardScaler()
 
 class ProteinCollection:
-    def __init__(self, contactmap: ContactMapper, pdb_ID: str, pdb_mutations: dict):
+    """
+    Class that captures protein properties and observed values,
+    computed covariance matrices
+    """
+    def __init__(self, contactmap: ContactMapper, pdb_ID: str, 
+            mutations_exp: dict={}, mutations_sim: dict={}):
         self.pdb_ID = pdb_ID
         self.contactmap = contactmap
         self.adjacency = contactmap.adjacency
         self.sequence = contactmap.sequence
         assert self.pdb_ID == self.contactmap.pdb_ID
-        self.mutation_dict = pdb_mutations.get(pdb_ID)
+        self.mutations_exp = mutations_exp.get(pdb_ID)
+        self.mutations_sim = mutations_sim.get(pdb_ID)
         self.mutation_ids = ["WT"]
         self.ΔΔg = [0]
         self.kernel_factory = KernelFactory()
-        self.mutated_sequences, self.mutated_adjacencies = self.derive_mutations()
+        self.mut_S_exp, self.mut_adj_exp, self.mut_S_is, self.mut_adj_is = self.derive_mutations()
+        self.mutated_sequences = self.mut_S_exp + self.mut_S_is
+        self.mutated_adjacencies = self.mut_adj_exp + self.mut_adj_is
+        self.ΔΔg_exp = self.ΔΔg[:len(self.mut_S_exp)]
+        self.ΔΔg_is = self.ΔΔg[len(self.mut_S_exp):]
         # TODO For testing compute once and save result as pickle
         if not isfile('test_wdk.pickle'):
             self.matrix_kernels: dict = self.compute_matrices()
@@ -38,9 +51,49 @@ class ProteinCollection:
         #self.matrix_kernels = self.compute_matrices()
         self.matrices_df: pd.DataFrame = self.generate_df_representation()
         self.mWDK = WeightedDecompositionKernel(kernels=self.matrices_df)
-        self.mwdk_df: pd.DataFrame = pd.DataFrame(self.mWDK.K_ϕ.detach().numpy(), index=self.mutation_ids, columns=self.mutation_ids)
+        mwdk_values = self.mWDK.K_ϕ.detach().numpy()
+        # TODO scaling quickfix
+        scaler.fit(mwdk_values)
+        scaled_mwdk_values = scaler.transform(mwdk_values)
+        ##
+        self.mwdk_df: pd.DataFrame = pd.DataFrame(scaled_mwdk_values, index=self.mutation_ids, columns=self.mutation_ids)
 
-    def parse_and_assert_mutations(self, mutation) -> Tuple[str, int, str]:
+    def derive_mutations(self) -> Tuple[list, list, list, list]:
+        """
+        parses mutations from mutation_dictionaries
+        mutates ΔΔg and mutation id as class properties
+        """
+        print("Parsing experimental mutations ...")
+        mut_S_exp, mut_adj_exp = self._parse_mutations(self.mutations_exp)
+        print("Parsing in silico mutations ...")
+        mut_S_is, mut_adj_is = self._parse_mutations(self.mutations_sim)
+        return mut_S_exp, mut_adj_exp, mut_S_is, mut_adj_is
+
+    def _parse_mutations(self, mutation_dict) -> Tuple[list, list]:
+        mutated_sequences = []
+        mutated_adjacencies = []
+        if not mutation_dict:
+            print("WARNING: No mutations provided.")
+            return mutated_sequences, mutated_adjacencies
+        for (mutation, ddg) in tqdm(mutation_dict):
+            self.ΔΔg.append(ddg)
+            self.mutation_ids.append(mutation)
+            # deepcopy to ensure that the underlying wildtype is not overwritten
+            sequence = deepcopy(self.sequence)
+            adjacency = deepcopy(self.adjacency)
+            mutation_tuples = self._parse_and_assert_mutations(mutation)
+            for _, idx, mut in mutation_tuples:
+                sequence[idx] = mut
+                # change imutable reference tuple by creating new tuple
+                adjacency[idx] = (mut, adjacency[idx][1])
+            mutated_sequences.append(sequence)
+            mutated_adjacencies.append(adjacency)
+        return mutated_sequences, mutated_adjacencies
+
+    def _parse_and_assert_mutations(self, mutation) -> Tuple[str, int, str]:
+        """
+        decompose and handle mutliple mutations (not single point)
+        """
         mutation_tuples = []
         s_mutations = re.split(r'(\d+)([A-Z])', mutation)[:-1]
         for i in range(0, len(s_mutations), 3):
@@ -52,38 +105,18 @@ class ProteinCollection:
             mutation_tuples.append((seq_res, seq_idx, seq_mut))
         return mutation_tuples
 
-    def derive_mutations(self) -> list:
-        """
-        parses mutations from mutation_dictionary
-        mutates ΔΔg and mutation id as class properties
-        """
-        mutated_sequences = []
-        mutated_adjacencies = []
-        for (mutation, ddg) in self.mutation_dict:
-            self.ΔΔg.append(ddg)
-            self.mutation_ids.append(mutation)
-            # deepcopy to ensure that the underlying wildtype is not overwritten
-            sequence = deepcopy(self.sequence)
-            adjacency = deepcopy(self.adjacency)
-            mutation_tuples = self.parse_and_assert_mutations(mutation)
-            for _, idx, mut in mutation_tuples:
-                sequence[idx] = mut
-                # change imutable reference tuple by creating new tuple
-                adjacency[idx] = (mut, adjacency[idx][1])
-            mutated_sequences.append(sequence)
-            mutated_adjacencies.append(adjacency)
-        return mutated_sequences, mutated_adjacencies
-
     def compute_matrices(self) -> dict:
         """
-        evaluate all mutations with one-another
+        compute substitution over all mutations with one-another
         """
+        print("Computing kernel matrices ...")
         wdks = {kernel: {} for kernel in self.kernel_factory.sub_matrices}
         for k_name, kernel in zip(self.kernel_factory.sub_matrices, self.kernel_factory.kernels):
             sequences = [self.sequence] + self.mutated_sequences
             adjacencies = [self.adjacency] + self.mutated_adjacencies
-            for idx, (p_seq, p_adj) in enumerate(zip(sequences, adjacencies)):
-                print(f"MUTATION {self.mutation_ids[idx]}") # N = wt+mutations
+            pbar = tqdm(enumerate(zip(sequences, adjacencies)))
+            for idx, (p_seq, p_adj) in pbar:
+                pbar.set_description(f"MUTATION {self.mutation_ids[idx]}") # N = wt+mutations
                 mat_vals = []
                 for q_seq, q_adj in zip(sequences[idx:], adjacencies[idx:]):
                     # set object properties before computation
@@ -135,16 +168,19 @@ class ProteinCollection:
                 ax[idx].set_yticks([])
             ax[idx].set_title("{}".format(self.kernel_factory.sub_matrices[idx]))
         plt.savefig("./fig/mat_viz.png")
+        plt.legend()
         plt.show()
 
     def plot_mwdk(self):
         _, ax = plt.subplots(1, 1, figsize=(20, 10))
-        ax.imshow(self.mwdk_df.to_numpy())
+        im = ax.imshow(self.mwdk_df.to_numpy())
         ax.set_yticks(np.arange(len(self.mwdk_df.columns)))
         ax.set_xticks(np.arange(len(self.mwdk_df.columns)))
         ax.set_yticklabels(self.mwdk_df.columns, size=5)
         ax.set_xticklabels(self.mwdk_df.columns, rotation=90, size=5)
         ax.set_title("mWDK values")
+        cbar = ax.figure.colorbar(im, ax=ax)
+        cbar.ax.set_ylabel("", rotation=-90, va="bottom")
         plt.savefig("./fig/mwdk.png")
         plt.show()
 
