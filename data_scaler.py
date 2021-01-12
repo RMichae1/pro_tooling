corrected sigma calculation, plotting, set evaluation of theta over range of parameters

data_scaler.py
@@ -17,12 +17,14 @@ class BayesScaler:
     """
     def __init__(self, is_mutations, ΔΔg, exp_mutations, experimentally_observed_ΔΔg,
                     α_a=2., β_a=1.5, α_b=1.3, β_b=2., α_c=2, β_c=5., σ_d=0.15, σ_n=0.5,
-                    samples_N=10000, warmup_N=500, TESTING=False):
+                    samples_N=10000, warmup_N=500, TESTING=False, pdb_ID=None):
         pyro.set_rng_seed(42)
         pyro.clear_param_store()
+        self.pdb_ID = pdb_ID
         self.samples_N = samples_N if not TESTING else 1000
         self.warmup_N = warmup_N
         self.chains_N = 1
+        self.x_range = (-10, 7)
         self.is_mutations = is_mutations
         self.exp_mutations= exp_mutations
         self.ΔΔg_is = ΔΔg
@@ -40,34 +42,29 @@ class BayesScaler:
         self.α_b, self.β_b = α_a, β_a
         self.α_c, self.β_c = α_c, β_c
         self.σ_d, self.σ_n = σ_d, σ_n
-        ## DEV TEST
-        if not isfile('test_mcmc.pickle'):
-            self.mcmc = self.run_mcmc()
-            with open('test_mcmc.pickle', 'wb') as outfile:
-                pickle.dump(self.mcmc.get_samples(), outfile)
-        ## DEV TEST save time on compute mcmc
-        else:
-            with open('test_mcmc.pickle', 'rb') as infile:
-                self.mcmc = pickle.load(infile)
-        ## END DEV
+        self.mcmc = self.run_mcmc()
 
-        # self.mcmc_samples = {k: v.detach().cpu().numpy() for k, v in self.mcmc.get_samples().items()}
-        # a = self.mcmc_samples.get('a').mean(0)
-        # b = self.mcmc_samples.get('b').mean(0)
-        # c = self.mcmc_samples.get('c').mean(0)
-        # d = self.mcmc_samples.get('d').mean(0)
+        self.mcmc = {k: v.detach().cpu().numpy() for k, v in self.mcmc.get_samples().items()}
+        self.a_samples = self.mcmc.get("a")
+        self.b_samples = self.mcmc.get("b")
+        self.c_samples = self.mcmc.get("c")
+        self.d_samples = self.mcmc.get("d")
+        self.a = self.a_samples.mean(0)
+        self.b = self.b_samples.mean(0)
+        self.c = self.c_samples.mean(0)
+        self.d = self.d_samples.mean(0)
 
-        self.a = self.mcmc.get('a').mean(0).numpy()
-        self.b = self.mcmc.get('b').mean(0).numpy()
-        self.c = self.mcmc.get('c').mean(0).numpy()
-        self.d = self.mcmc.get('d').mean(0).numpy()
+        # put samples in range context:
+        self.xx = np.arange(self.x_range[0], self.x_range[1], 0.01)
+        # theta for all sampled points
+        self.θ_samples = np.array([self.a_samples[i] * np.exp(np.dot(self.c_samples[i], 
+                self.ΔΔg_is)) + np.dot(self.b_samples[i], self.ΔΔg_is) + self.d_samples[i] for i in range(self.samples_N)])
+        self.θ_xx = np.array([self.a_samples[i] * np.exp(np.dot(self.c_samples[i], 
+                self.xx)) + np.dot(self.b_samples[i], self.xx) + self.d_samples[i] for i in range(self.samples_N)])
         self.θ = self.a * np.exp(np.dot(self.c, self.ΔΔg_is)) + np.dot(self.b, self.ΔΔg_is) + self.d
-        print(f"theta shape: {self.θ.shape}")
-        # TODO see if theta is over individual samples
-        self.θ_mean = np.mean(self.θ)
-        print(self.θ_mean)
-        self.σ_T = np.sum(np.square(self.θ - self.θ_mean)) #/ len(self.θ) # TODO check correctness w.r.t. gp_modeling
-        print(self.σ_T)
+        self.σ_T = np.sum(np.square(self.θ_samples - self.θ), axis=1) / self.samples_N
+        # compute sigma over the whole range of possible inputs
+        self.σ_T_sampled = np.sum(np.square(self.θ_xx - np.array(list(map(self.transform, self.xx)))), axis=1) / self.samples_N
 
     def transform(self, x: float) -> float:
         return self.a * np.exp(np.dot(self.c, x)) + np.dot(self.b, x) + self.d
@@ -111,7 +108,7 @@ class BayesScaler:
         return mcmc
 
     @staticmethod
-    def summary(samples):
+    def summary(samples: dict) -> dict:
         """
         Utility function to print latent sites' quantile information.
         """
@@ -122,38 +119,55 @@ class BayesScaler:
             site_stats[site_name] = describe[["mean", "std", "5%", "25%", "50%", "75%", "95%"]]
         return site_stats
 
+    def _sample_f_suggestions(self, x: np.ndarray, n_samples:int=1000) -> list:
+        """
+        From fitted values sample function suggestions for plotting
+        """
+        pass
+        # a = dist.Normal(self.mcmc.get('a').mean(0), self.mcmc.get('a').std(0))
+        # b = dist.Normal(self.mcmc.get('b').mean(0), self.mcmc.get('b').std(0))
+        # c = dist.Normal(self.mcmc.get('c').mean(0), self.mcmc.get('c').std(0))
+        # d = dist.Normal(self.mcmc.get('d').mean(0), self.mcmc.get('d').std(0))
+        # y_list = [a().numpy() * np.exp(np.dot(c().numpy(), x)) + np.dot(b().numpy(), x) + d().numpy() 
+        #             for _ in range(n_samples)]
+        # return y_list
+
     def print_summary(self):
         #for site, values in self.summary(self.mcmc_samples).items():
         for site, values in self.summary(self.mcmc).items():
             print("Site: {}".format(site))
             print(values, "\n")
 
-    def plot_scaling(self):
+    def plot_scaling(self, save_fig="./fig/"):
+        filename = f"{save_fig}/bayes_scaling_{self.pdb_ID}.png"
         fig, ax = plt.subplots(1,2 ,figsize=(25,10))
-        sns.scatterplot(x=self.ΔΔg_is.numpy(), y=self.θ, ci=self.σ_T, color=".2", marker=".", ax=ax[0])
-        sns.scatterplot(x=self.ΔΔg_is.numpy(), y=self.ΔΔg_exp.numpy(), s=100, color="blue", ax=ax[0])
-        # TODO figure out how to plot all sampled points
-        # ...
-        xx = np.arange(-10, 7, 0.1)
-        y = list(map(self.transform, xx))
-        # transformed values
-        ax[0].plot(xx, y, "k-", alpha=0.5)
-        ci_pos = self.θ + self.σ_T * 2
-        ci_neg = self.θ - self.σ_T * 2
-        ax[0].plot(self.ΔΔg_is.numpy(), ci_pos, "r.") # TODO order points for smoother plotting
-        ax[0].plot(self.ΔΔg_is.numpy(), ci_neg, "r.")
+        # plot sampled theta for background
+        for y in self.θ_xx[:-int(0.1*self.samples_N)]:
+            ax[0].plot(self.xx, y, "k-", alpha=0.002)
+        # plot final theta over complete range
+        y = list(map(self.transform, self.xx))
+        ax[0].plot(self.xx, y, "k-", label="μ scaling")
+        sns.scatterplot(x=self.ΔΔg_is.numpy(), y=self.θ, ci=self.σ_T, s=20, color="red", ax=ax[0], label="scaled simulated data")
+        sns.scatterplot(x=self.ΔΔg_is.numpy(), y=self.ΔΔg_exp.numpy(), s=250, color="blue", ax=ax[0], label="experimental")
+        ci_pos = self.θ + self.σ_T
+        ci_neg = self.θ - self.σ_T
+        # draw confidence intervals at val +/- one σ_T
+        ordered_val_ci_pos = np.array(sorted(list(zip(self.ΔΔg_is.numpy(), ci_pos)), key=lambda x: x[0], reverse=True))
+        ordered_val_ci_neg = np.array(sorted(list(zip(self.ΔΔg_is.numpy(), ci_neg)), key=lambda x: x[0], reverse=True))
+        ax[0].plot(ordered_val_ci_pos[:, 0], ordered_val_ci_pos[:, 1], "r--") # TODO order points for smoother plotting
+        ax[0].plot(ordered_val_ci_neg[:, 0], ordered_val_ci_neg[:, 1], "r--")
         # TODO add green interval for sampling posterior
         # barplot over scaled y values
         # TODO figure out shape sigma
-        #sns.histplot(self.σ_T, ax=ax[1], label="σ_T distribution", stat="density", log_scale=True)
+        sns.histplot(self.σ_T_sampled, ax=ax[1], label="σ_T distribution", stat="density")
         #ax[1].bar(x=self.σ_T, height=self.θ, width=0.2, label="θ values")
         ax[0].set_xlabel("ΔΔG original")
         ax[1].set_xlabel("σT")
         ax[0].set_ylabel("ΔΔG yE, yS")
         ax[0].set_ylim((-12, 7))
-        ax[0].set_xlim((-10, 7))
-        plt.suptitle("Stability Transformation")
-        plt.savefig("./fig/bayes_scaling.png")
+        ax[0].set_xlim(self.x_range)
+        plt.suptitle(f"Stability Transformation {self.pdb_ID}")
+        plt.savefig(filename)
         plt.legend()
         plt.show()
        