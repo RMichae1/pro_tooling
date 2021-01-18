97e1bef6199b2babb8ba7e84d54a94c94dcbaceb
Richard Michael <richard.michael.1995@gmail.com>
2021-01-18 20:07:29 +0100

    Corrected test dimensions

--- test/test_predictions.py
+++ test/test_predictions.py
@@ -29,29 +29,19 @@ def f(y):
 
 ref_K = ref_file["Kfull"]
 num_exp_obs = ref_K.shape[0] - sigma_T.shape[0] - 1 
-contact_graph, X_wt, y_wt, X_exp, y_exp, X_is, y_is, y_train_exp_match, y_is_match, X_test_, _ = get_split_training_and_test_data("1PGA", 
+contact_graph, X_wt, y_wt, X_exp, y_exp, X_is, y_is, y_train_exp_match, y_is_match, X_test_, y_test = get_split_training_and_test_data("1PGA", 
                                                                                                     cutoff_distance=5., p=np.arange(num_exp_obs))
 X_test = torch.Tensor(X_test_).type(torch.float64)
 X_is = X_is[:20, :]
 y_is = y_is[:20, :]
+def test_parsed_ys_against_target():
+    assert np.vstack([y_wt, y_exp, y_is, y_test]).shape[0] == ref_file["training_targets"].shape[0]
 
 mut_exp = parse_matlab_mutation_file("./data/ddg_protherm.mat", query="ddg_protherm")
 mut_is = parse_matlab_mutation_file("./data/ddg_rosetta_single.mat", query="ddg_rosetta_single")
 cm = ContactMapper(pdb_file="./pdb/1pga.pdb", tri_dist=True)
 cm.adjacency = contact_graph # set adjacency here as well for downstream consistency
 prot = ProteinCollection(cm, pdb_ID="1PGA", mutations_exp=mut_exp, mutations_sim=mut_is)
-mut_S_exp, mut_adj_exp, ΔΔg_exp, mut_ids_exp = parse_mutations(mutation_dict=mut_exp.get(prot.pdb_ID), 
-                                                sequence=prot.sequence, adjacency=contact_graph)
-mut_S_is, mut_adj_is, ΔΔg_is, mut_ids_is = parse_mutations(mutation_dict=mut_is.get(prot.pdb_ID), 
-                                                sequence=prot.sequence, adjacency=contact_graph)
-# # select only for 20 insilico mutations
-#X_exp_train = convert_aa_sequence(mut_S_exp)
-#X_exp_test = convert_aa_sequence(mut_S_exp)
-# X_is = convert_aa_sequence(mut_S_is[:20])
-# X_wt = convert_aa_sequence([prot.sequence])
-# y_wt = np.array([prot.ΔΔg[0]])[:, np.newaxis]
-# y_exp = np.array(ΔΔg_exp)[:, np.newaxis]
-# y_is = np.array(ΔΔg_is[:20])[:, np.newaxis]
 
 y_scaled = f(y_is)
 mean_y, max_y, y_wt, y_exp, y_scaled = preprocess_observations(y_wt, y_exp, y_scaled)
@@ -59,21 +49,24 @@ mean_y, max_y, y_wt, y_exp, y_scaled = preprocess_observations(y_wt, y_exp, y_sc
 def test_predict_ymax():
     assert max_y == pytest.approx(ref_file["model"]["ymax"][0, 0][0, 0], rel=0.02)
 
-model = GPRegression(prot, X_wt.reshape(1, len(X_wt)), X_exp, X_is, y_wt, y_exp, y_scaled, max_y, mean_y, contact_graph, torch.Tensor(sigma_T))
+model = GPRegression(prot, X_wt.reshape(1, len(X_wt)), np.vstack([X_test_, X_exp]), X_is, 
+            y_wt, np.vstack([y_test, y_exp]), y_scaled, max_y, mean_y, contact_graph, torch.Tensor(sigma_T))
 # set model parameters to account for training/testing specs
-model.set_train_index(np.concatenate([np.array([0]), np.arange(21, model.X.shape[0])]))
+model.set_train_index(np.concatenate([np.array([0]), np.arange(21, model.y.shape[0])]))
 model.set_test_index(np.arange(1, 21))
 
-assert model.X_train.shape[0] == len(model.idx_train)
-assert np.vstack([X_wt, model.X_exp[20:,:], X_is]).shape[0] == len(model.idx_train)
-assert np.vstack([y_wt, model.y_exp[20:,:], y_is]).shape[0] == len(model.idx_train) 
-assert X_test_.shape[0] == len(model.idx_test)
+def test_model_dimensions():
+    assert model.X_train.shape[0] == len(model.idx_train)
+    assert np.vstack([X_wt, model.X_exp[20:,:], X_is]).shape[0] == len(model.idx_train)
+    assert len(model.y_train) == len(model.idx_train) 
+    assert X_test_.shape[0] == len(model.idx_test)
 
 def test_targets():
     np.testing.assert_allclose(model.y[0, :], ref_file["training_targets"][0, :], atol=0.01)
-    np.testing.assert_allclose(model.y[1:, :], ref_file["training_targets"][1:, :], atol=0.015)
+    assert model.y.shape[0] == ref_file["training_targets"].shape[0]
+    np.testing.assert_allclose(model.y[0:, :], ref_file["training_targets"][0:, :], atol=0.015)
 
-noise = model.set_noise_term().detach().numpy()#[model.idx_train]
+noise = model.set_noise_term().detach().numpy()[model.idx_train]
 
 def test_noise_equal():
     np.testing.assert_almost_equal(noise[0], ref_noise[0])
@@ -97,7 +90,8 @@ K[1:, 1:] = ref_K[21:, 21:]
 #     np.testing.assert_allclose(ref_mean_pred_local, ref_unscaled_pred)
 
 def test_mean_prediction_against_ref():
-    ref_mean_corrected = np.linalg.solve(K, KZX_ref.T).T.dot(model.y)
+    print(model.y.shape)
+    ref_mean_corrected = np.linalg.solve(K, KZX_ref.T).T.dot(model.y_train)
     ref_mean_corrected *= max_y
     ref_mean_corrected += mean_y
     mu, _ = model._fit()
