f8b4b11c310efec1879b4faf1d7b2e7a1a7447a3
Richard Michael <richard.michael@zapliance.de>
2021-02-23 15:35:32 +0100

    added Pyro VAE and training routines

---
+++ run_vae.py
@@ -0,0 +1,70 @@
+from vae import VAE
+import pickle
+from utility import one_hot_encoding
+from vae import train, evaluate
+from pyro.infer import SVI, JitTrace_ELBO
+from pyro.optim import Adam
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import torch
+from tqdm import tqdm
+
+if __name__ == "__main__":
+    with open("./data/BLAT_data_df.pkl", "rb") as infile:
+        blat_df = pickle.load(infile)
+    # stored values without assay entries are BLAT TEM1 ECOLX
+    blat_df = blat_df[blat_df.assay.isna()]
+    # cast sequence labels to int
+    all_seqs = []
+    for seq in blat_df.seqs:
+        all_seqs.append([int(elem) for elem in seq])
+    all_seqs = np.array(all_seqs)
+    x, y = all_seqs.shape
+    one_seq = all_seqs.reshape(x*y, )
+    one_hot_sequence = one_hot_encoding(one_seq).reshape(x, y, 23)
+
+    seq_dataset = torch.utils.data.TensorDataset(torch.tensor(one_hot_sequence,
+                                                              dtype=torch.float))
+    test_size = int(0.1 * x)
+    seq_train, seq_test = torch.utils.data.random_split(seq_dataset, [ x - test_size, test_size])
+    batch_size = 128
+    train_loader = torch.utils.data.DataLoader(seq_train, batch_size=batch_size)
+    test_loader = torch.utils.data.DataLoader(seq_test, batch_size=batch_size)
+
+    LEARNING_RATE = 5e-3
+    USE_CUDA = False
+    NUM_EPOCHS = 1000
+    TEST_FREQ = 10
+    INPUT_DIM = int(one_hot_sequence.shape[1] * one_hot_sequence.shape[2])
+
+    vae = VAE(z_dim=100, hidden_dim=200, input_dims=INPUT_DIM, use_cuda=USE_CUDA)
+    optimizer = Adam({"lr": LEARNING_RATE})
+    svi = SVI(vae.model, vae.guide, optimizer, loss=JitTrace_ELBO())
+
+    train_elbo = []
+    test_elbo = []
+    for epoch in tqdm(range(NUM_EPOCHS)):
+        total_epoch_loss_train = train(svi, train_loader, USE_CUDA)
+        train_elbo.append(-total_epoch_loss_train)
+        print(f"[epoch {epoch}] avrg. train loss: {total_epoch_loss_train}")
+
+        if epoch % TEST_FREQ == 0:
+            total_epoch_loss_test = evaluate(svi, test_loader, USE_CUDA)
+            test_elbo.append(-total_epoch_loss_test)
+            print(f"[epoch {epoch}] avrg. test loss: {total_epoch_loss_test}")
+
+    plt.plot(train_elbo, label="train")
+    plt.legend()
+    plt.title("ELBO Training over iterations")
+    plt.ylabel("neg. training loss [ELBO]")
+    plt.xlabel("validation step")
+    plt.show()
+
+    plt.plot(test_elbo, label="test")
+    plt.legend()
+    plt.title("ELBO validation over validation-steps")
+    plt.ylabel("neg. validation loss [ELBO]")
+    plt.xlabel("validation steps")
+    plt.xticks(np.arange(0, int(NUM_EPOCHS / TEST_FREQ) + .1, step=1))
+    plt.show()
---
+++ vae.py
@@ -0,0 +1,97 @@
+import pyro
+import pyro.distributions as dist
+import pandas as pd
+import torch
+from torch import nn
+pyro.enable_validation()
+
+
+class Encoder(nn.Module):
+    def __init__(self, z_dim, hidden_dim, input_dims):
+        super().__init__()
+        self.sequence_dims = input_dims
+        self.fc1 = nn.Linear(input_dims, hidden_dim)
+        self.fc21 = nn.Linear(hidden_dim, z_dim)
+        self.fc22 = nn.Linear(hidden_dim, z_dim)
+        self.softplus = nn.Softplus()
+
+    def forward(self, x):
+        x = x.reshape(-1, self.sequence_dims)
+        hidden = self.softplus(self.fc1(x))
+        z_loc = self.fc21(hidden)
+        z_scale = torch.exp(self.fc22(hidden))
+        return z_loc, z_scale
+
+
+class Decoder(nn.Module):
+    def __init__(self, z_dim, hidden_dim, input_dims):
+        super().__init__()
+        self.fc1 = nn.Linear(z_dim, hidden_dim)
+        self.fc21 = nn.Linear(hidden_dim, input_dims)
+        self.softplus = nn.Softplus()
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, z):
+        hidden = self.softplus(self.fc1(z))
+        loc_img = self.sigmoid(self.fc21(hidden))
+        return loc_img
+
+
+class VAE(nn.Module):
+    def __init__(self, z_dim, hidden_dim, input_dims, use_cuda=False):
+        super().__init__()
+        self.input_dims = input_dims
+        self.encoder = Encoder(z_dim, hidden_dim, input_dims)
+        self.decoder = Decoder(z_dim, hidden_dim, input_dims)
+
+        if use_cuda:
+            self.cuda()
+        self.use_cuda = use_cuda
+        self.z_dim = z_dim
+
+    def model(self, x):
+        pyro.module("decoder", self.decoder)
+        with pyro.plate("data", x.shape[0]):
+            z_loc = x.new_zeros(torch.Size((x.shape[0], self.z_dim)))
+            z_scale = x.new_ones(torch.Size((x.shape[0], self.z_dim)))
+            z = pyro.sample("latent", dist.Normal(z_loc, z_scale).to_event(1))
+            loc_seq = self.decoder.forward(z)
+            pyro.sample("obs", dist.Bernoulli(loc_seq, validate_args=True).to_event(1), obs=x.reshape(-1,
+                                                                                                       self.input_dims))
+
+    def guide(self, x):
+        pyro.module("encoder", self.encoder)
+        with pyro.plate("data", x.shape[0]):
+            z_loc, z_scale = self.encoder.forward(x)
+            pyro.sample("latent", dist.Normal(z_loc, z_scale).to_event(1))
+
+    def reconstruct_seq(self, x):
+        z_loc, z_scale = self.encoder(x)
+        z = dist.Normal(z_loc, z_scale).sample()
+        loc_seq = self.decoder(z)
+        return loc_seq
+
+
+def train(svi, train_loader, use_cuda=False):
+    epoch_loss = 0
+    for tensor_list in train_loader:
+        for x in tensor_list:
+            if use_cuda:
+                x = x.cuda()
+            epoch_loss += svi.step(x)
+    normalizer_train = len(train_loader.dataset)
+    total_epoch_loss_train = epoch_loss / normalizer_train
+    return total_epoch_loss_train
+
+
+def evaluate(svi, test_loader, use_cuda=False):
+    test_loss = 0
+    for tensor_list in test_loader:
+        for x in tensor_list:
+            if use_cuda:
+                x = x.cuda()
+            test_loss += svi.evaluate_loss(x)
+    normalizer_test = len(test_loader.dataset)
+    total_epoch_loss_train = test_loss / normalizer_test
+    return total_epoch_loss_train
+
