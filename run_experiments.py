66692c0769136dc18a972ff6376cef4a1e83cbe2
Richard Michael <richard.michael.1995@gmail.com>
2021-02-24 12:56:42 +0100

    added BLAT pos lvl CV loop

--- run_experiments.py
+++ run_experiments.py
@@ -80,18 +80,20 @@ def init_experiment_run(pdb: str) -> tuple:
     # scale using Bayesian Scaling
     bs_rosetta = BayesScaler(is_mutations=mut_ids_is, ΔΔg=pcol.ΔΔg_is, exp_mutations=mut_ids_exp, 
                         experimentally_observed_ΔΔg=pcol.ΔΔg_exp, TESTING=False, pdb_ID=pdb)
-    bs_rosetta.plot_scaling()
+    σ_T = bs_rosetta.σ_T
+
     ΔΔg_exp = ΔΔg_exp[:, np.newaxis]
     ΔΔg_is_scaled = bs_rosetta.transform(ΔΔg_is)[:, np.newaxis]
     # Scale y-values as done in the implementation by normalizing with mean and max
     mean_y, max_y, y_wt, ΔΔg_exp, ΔΔg_is_scaled = preprocess_observations(y_wt, ΔΔg_exp, ΔΔg_is_scaled)
-    return pcol, X_wt, X_exp, X_is, y_wt, ΔΔg_exp, ΔΔg_is_scaled, ref_adj, bs_rosetta, max_y, mean_y
+    return pcol, X_wt, X_exp, X_is, y_wt, ΔΔg_exp, ΔΔg_is_scaled, ref_adj, σ_T, max_y, mean_y
+
 
 def init_mgp_regression(pcol, X_wt, X_exp, X_is, y_wt, ΔΔg_exp, ΔΔg_is_scaled, ref_adj, 
-                    bs_rosetta, max_y, mean_y):
+                    σ_T, max_y, mean_y, fusion=True):
     gpr = GPRegression(protein_representation=pcol, X_wt=X_wt, X_exp=X_exp, X_is=X_is, 
                          y_wt=y_wt, y_exp=ΔΔg_exp, y_is=ΔΔg_is_scaled, adjacencies=ref_adj, 
-                         σ_T=bs_rosetta.σ_T, y_max=max_y, y_mean=mean_y, cached=True)
+                         σ_T=σ_T, y_max=max_y, y_mean=mean_y, cached=True, fusion=fusion)
     return gpr
 
 def cached_pos_lvl_CV(pdb, reference: bool=False, optim: bool=True) -> dict:
@@ -224,19 +226,17 @@ def run_mgpfusion_experiment(pdb):
     write_results(gpr_results_mutation_lvl, dir="mGPfusion/mut_cv", suffix="mut_lvl_no_optim_REF")
 
 
-def run_BLAT_experiment():
+def cached_BLAT_pos_lvl_CV(reference: bool=False, optim: bool=True) -> dict:
     blat_file = os.path.join(os.path.dirname(__file__), os.path.join("data/blat/BLAT_ECOLX_Ranganathan2015.csv"))
     blat_df = pd.read_csv(blat_file)
     blat_df["growth"] = blat_df["2500"]
     clipped_mutations = [(mut, growth) for (mut, growth) in zip(blat_df.mutant, blat_df.growth) if int(mut[1:-1])<=263]
     # WARNING: we clip mutations at position 263 - mutations go until 286, however pdb is only 263 (main chain) long
     mutation_dict = {"1FQG" : clipped_mutations}
-    
     pdb_file = "./pdb/1fqg.pdb"
     contact_map = ContactMapper(pdb_file=pdb_file, tri_dist=True)
     # contact_map.plot_distance_matrix()
     # contact_map.plot_contact_map()
-
     pcol = ProteinCollection(contact_map, pdb_ID="1FQG", mutations_exp=mutation_dict, mutations_sim={})
     adjacencies = contact_map.adjacency
     mut_S_exp, mut_adj_exp, ΔΔg_exp, mut_ids_exp = parse_mutations(mutation_dict=mutation_dict.get(pcol.pdb_ID), 
@@ -244,25 +244,72 @@ def run_BLAT_experiment():
     X_exp = convert_aa_sequence(mut_S_exp)
     y_wt = np.array([0.])[:, np.newaxis]
     X_wt = convert_aa_sequence([pcol.sequence])
-
     ΔΔg_exp = np.array(ΔΔg_exp)[:, np.newaxis]
     ΔΔg_is = np.array([])[:, np.newaxis]
 
      # Scale y-values as done in the implementation by normalizing with mean and max
     mean_y, max_y, y_wt, ΔΔg_exp, ΔΔg_is_scaled = preprocess_observations(y_wt, ΔΔg_exp,  ΔΔg_is)
 
-    gpr = GPRegression(protein_representation=pcol, X_wt=X_wt, X_exp=X_exp, X_is=np.array([]), 
-                        y_wt=y_wt, y_exp=ΔΔg_exp, y_is=ΔΔg_is_scaled, adjacencies=adjacencies, 
-                        σ_T=torch.Tensor([0.]), y_max=max_y, y_mean=mean_y, fusion=False, cached=True)
-    
-    gpr_results_mutation_lvl = gpr.mutation_level_CV(ref=False, optim=False)
-    write_results(gpr_results_mutation_lvl, gpr, dir="mGPfusion/blat/mut_cv", suffix="_no_optim")
-    gpr_results_mutation_lvl = gpr.mutation_level_CV(ref=False, optim=True)
-    write_results(gpr_results_mutation_lvl, gpr, dir="mGPfusion/blat/mut_cv", suffix="_")
-    gpr_results_pos_lvl = gpr.position_level_CV(ref=False, optim=False)
-    write_results(gpr_results_pos_lvl, gpr, dir="mGPfusion/blat/pos_cv", suffix="_no_optim")
-    gpr_results_pos_lvl = gpr.position_level_CV(ref=False, optim=True)
-    write_results(gpr_results_pos_lvl, gpr, dir="mGPfusion/blat/pos_cv", suffix="_")
+    mutations = []
+    optimization_parameters = []
+    fit_parameters = []
+    for pos in tqdm(range(len(pcol.sequence))):
+        print("reinitialize ...")
+        gpr = init_mgp_regression(pcol, X_wt, X_exp, np.array([]), y_wt, ΔΔg_exp, ΔΔg_is_scaled, adjacencies, 
+                                σ_T=torch.Tensor([0.]), max_y=max_y, mean_y=mean_y, fusion=False)
+        experimental_mutation_index = get_mutation_idx(gpr.protein.mut_ids_exp)
+        # gather all mutations at that position and assign train and test indices
+        mutation_bool_mask = np.array([bool(pos in mut) for mut in experimental_mutation_index])
+        test_mutation_idx = np.where(mutation_bool_mask)[0]
+        not_test_mutation_idx = np.where(~mutation_bool_mask)[0]
+        n_mutations = np.array([len(mut) for mut in experimental_mutation_index if bool(pos in mut)])
+        # split into train and test
+        gpr.set_test_index(1+test_mutation_idx) # offset with WT 
+        # combine WT + not selected + in silico for training data
+        train_index = np.concatenate([np.array([0]), 1+not_test_mutation_idx, 
+                        np.arange(start=len(gpr.X_exp)+1, stop=gpr.X.shape[0])]) # all simulated data are training data
+        gpr.set_train_index(train_index)
+        if gpr.x_test.shape[0] == 0:
+            print(f"No Mutation at pos:{pos} - skipping...")
+            continue
+        # optimize
+        nll_init = gpr.neg_ll()
+        if optim:
+            try:
+                gpr.parameter_optimization()
+            except RuntimeError as _:
+                print("Optimization broke.")
+                gpr.reset_trainable_parameters()
+        nll_end = gpr.neg_ll()
+        f_μ, cov = gpr._fit(ref=reference)
+        # write optimization results
+        optimization_parameters.append({"w": gpr.weights.get_value(),
+                                    "sigma_S": gpr.σ_S.get_value(),
+                                    "sigma_E": gpr.σ_E.get_value(),
+                                    "t": gpr.t.get_value(),
+                                    "nll": (nll_init, nll_end)})
+        mutations.append(n_mutations)
+        fit_parameters.append({'mu': f_μ.squeeze().detach().numpy(),
+                                'cov': cov.squeeze().detach().numpy(),
+                                'y_exp': (gpr.y_test.detach().numpy() * gpr.y_max) + gpr.y_mean
+                                })
+    predictions = np.concatenate([np.atleast_1d(x) for x in [elem.get('mu') for elem in fit_parameters]])
+    experimental = np.concatenate([x for sub in [elem.get('y_exp') for elem in fit_parameters] for x in sub])
+    rho = compute_ρ(y_vec=experimental, y_pred_μ=predictions)
+    rmse = compute_rmse(y=experimental, y_pred_μ=predictions)
+    results = {"optimization": optimization_parameters, 
+                "regression": fit_parameters, 
+                "mutations": mutations,
+                "rho": rho,
+                "rmse": rmse}
+    return results
+
+
+def run_BLAT_experiment():    
+    gpr_results_pos_lvl = cached_BLAT_pos_lvl_CV(reference=False, optim=False)
+    write_results(gpr_results_pos_lvl, dir="mGPfusion/blat/pos_cv", cv="pos_lvl_no_optim", pdb="1FQG")
+    gpr_results_pos_lvl = cached_BLAT_pos_lvl_CV(reference=False, optim=True)
+    write_results(gpr_results_pos_lvl, dir="mGPfusion/blat/pos_cv", suffix="pos_lvl", pdb="1FQG")
 
 
 def run_BLAT_experiment_PALZKILL_1BTL():
