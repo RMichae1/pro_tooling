6b387cba7cd215f16b70676e31cff8382911031c
Richard Michael <richard.michael.1995@gmail.com>
2021-03-18 17:32:51 +0100

    evaluate against Ranganathan data

--- run_vae.py
+++ run_vae.py
@@ -1,7 +1,7 @@
 from vae import VAE
 import pickle
 import os
-from utility import one_hot_encoding
+from utility import aa2index, one_hot_encoding
 from vae import train, evaluate
 import pyro
 from pyro.infer import SVI, JitTrace_ELBO
@@ -9,6 +9,7 @@ from pyro.optim import Adam, ClippedAdam
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from scipy.stats import spearmanr
 import torch
 from tqdm import tqdm
 import logging
@@ -17,6 +18,10 @@ import argparse
 import mlflow
 import seaborn as sns
 import matplotlib.pyplot as plt
+
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True' #TODO figure out what caused OMP Error #15
+
 
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
@@ -46,29 +51,38 @@ if __name__ == "__main__":
 
     with open("./data/blat/BLAT_data_df.pkl", "rb") as infile:
         blat_df = pickle.load(infile)
-    # stored values without assay entries are BLAT TEM1 ECOLX
-    blat_df = blat_df[blat_df.assay.isna()]
+    # stored values without assay entries are BLAT TEM1 ECOLX family data
+    family_df = blat_df[blat_df.assay.isna()]
+    test_blat_df = blat_df[~blat_df.assay.isna()]
     # cast sequence labels to int
-    all_seqs = []
-    for seq in blat_df.seqs:
-        all_seqs.append([int(elem) for elem in seq])
-    all_seqs = np.array(all_seqs)
-    x, y = all_seqs.shape
-    categories = np.unique(all_seqs).shape[0]
-    one_seq = all_seqs.reshape(x*y, )
-    one_hot_sequence = one_hot_encoding(one_seq).reshape(x, y, categories)
+    family_seqs = np.array([[int(elem) for elem in seq] for seq in family_df.seqs])
+    test_blat_seqs = np.array([[int(elem) for elem in seq] for seq in test_blat_df.seqs])
+    test_y = np.array(test_blat_df.assay)
+
+    n, length = family_seqs.shape
+    test_n = test_blat_seqs.shape[0]
+    categories = np.unique(family_seqs).shape[0]
+    one_seq = family_seqs.reshape(n*length,)
+    test_one_seq = test_blat_seqs.reshape(test_n*length,)
+    # TODO refactor the one-hot encoding - this code is non-pythonic!
+    one_hot_sequence_all = one_hot_encoding(np.concatenate((one_seq, test_one_seq), axis=None))
+    one_hot_sequences = one_hot_sequence_all[:len(one_seq)].reshape(n, length, categories)
+    one_hot_test_sequences = one_hot_sequence_all[len(one_seq):].reshape(test_n, length, categories)
+    assert one_hot_sequences.shape[0] == len(family_df)
+    assert one_hot_test_sequences.shape[0] == len(test_blat_df)
 
     # load and encode data set
-    seq_dataset = torch.utils.data.TensorDataset(torch.tensor(one_hot_sequence,
+    seq_dataset = torch.utils.data.TensorDataset(torch.tensor(one_hot_sequences,
                                                               dtype=torch.float))
-    test_size = int(args.test_split * x)
-    seq_train, seq_test = torch.utils.data.random_split(seq_dataset, [ x - test_size, test_size])
+    test_seq_dataset = torch.utils.data.TensorDataset(torch.tensor(one_hot_test_sequences, 
+                                                                    dtype=torch.float))
+    test_size = int(args.test_split * n)
+    seq_train, seq_test = torch.utils.data.random_split(seq_dataset, [n - test_size, test_size])
     batch_size = args.batch_size
     train_loader = torch.utils.data.DataLoader(seq_train, batch_size=batch_size)
     test_loader = torch.utils.data.DataLoader(seq_test, batch_size=batch_size)
 
-    wt = torch.tensor(one_hot_sequence[0], dtype=torch.float)
-    seq_1 = torch.tensor(one_hot_sequence[1], dtype=torch.float)
+    WT = torch.tensor(one_hot_sequences[0], dtype=torch.float)
 
     # parameters
     param_dict = {"LEARNING_RATE": args.learn_rate,
@@ -77,7 +91,7 @@ if __name__ == "__main__":
             "TEST_FREQ": args.validate,
             "LATENT_DIM": args.latent_dim,
             "HIDDEN_DIM": args.hidden_dim,
-            "INPUT_DIM": int(one_hot_sequence.shape[1] * one_hot_sequence.shape[2]),
+            "INPUT_DIM": int(one_hot_sequences.shape[1] * one_hot_sequences.shape[2]),
             "test_split": args.test_split, 
             "batch_size": args.batch_size,
             "weight_decay": args.weight_decay,
@@ -91,7 +105,7 @@ if __name__ == "__main__":
     model_FILENAME = f"./models/VAE_z{args.latent_dim}_h{args.hidden_dim}_e{args.epochs}_d{args.dropout}.pt"
     optimizer_FILENAME = f"./models/Adam_z{args.latent_dim}_h{args.hidden_dim}_e{args.epochs}_d{args.dropout}.pt"
     vae = VAE(z_dim=param_dict["LATENT_DIM"], hidden_dim=param_dict["HIDDEN_DIM"], 
-                    input_dims=param_dict["INPUT_DIM"], use_cuda=args.cuda, wt=wt)
+                    input_dims=param_dict["INPUT_DIM"], use_cuda=args.cuda, wt=WT)
     optimizer = Adam({"lr": param_dict["LEARNING_RATE"], "weight_decay": param_dict["weight_decay"]})
     #optimizer = ClippedAdam({"lr": LEARNING_RATE})
     svi = SVI(vae.model, vae.guide, optimizer, loss=JitTrace_ELBO())
@@ -122,20 +136,35 @@ if __name__ == "__main__":
         mlflow.log_artifact(optimizer_FILENAME)
         mlflow.end_run()
     
-    print(vae.latent_sample(wt, n=3))
-    print(vae.mse_loss(wt))
-    # samples = []
-    # mse_diff = []
-    # for s in seq_dataset:
-    #     samples.append(vae.latent_sample(s[0], n=1).reshape(-1).detach().numpy())
-    #     mse_diff.append(vae.mse_diff(s[0]).detach().numpy())
+    wt_log_prob = vae.log_p(WT)[1].detach().numpy()
+    wt_elbo = vae.log_p(WT)[0].detach().numpy()
+    elbo_values = []
+    kld_values = []
+    log_likelihoods = []
+    samples = []
+    for s in test_seq_dataset:
+        samples.append(vae.latent_sample(s[0], n=1).reshape(-1).detach().numpy())
+        loss = vae.log_p(s[0])
+        elbo_values.append(loss[0].detach().numpy())
+        log_likelihoods.append(loss[1].detach().numpy())
+        kld_values.append(loss[2].detach().numpy())
+    # PLOT first two dimensions
     # samples = np.array(samples)
-    # print(samples)
-    # plt.scatter(samples[:, 0], samples[:, 1], c=mse_diff, alpha=0.125, s=1.5)
-    # plt.title("VAE z=2 latent representation")
+    # plt.scatter(samples[:, 0], samples[:, 1], c=log_likelihoods, alpha=0.25, s=1.5)
+    # plt.title("VAE z=20 latent representation on 2D")
     # plt.show()
-    print(vae.log_p(wt))
-    print(vae.log_p(seq_1))
-    print(vae.reconstruct(wt))
-    # TODO plot w/ hidden dim 2
+    # WT is first element
+    delta_log_p = [(l-wt_log_prob) for l in log_likelihoods]
+
+    fig, ax = plt.subplots(1, 1)
+    sns.regplot(delta_log_p, test_y, ax=ax[0], color="grey", scatter_kws={"alpha": 0.25})
+    ax[0].set_ylabel("measured growth (2500 ampicillin dose)")
+    ax[0].set_xlabel("delta log likelihood")
+    plt.suptitle("VAE loss to measured values \n (2500 ampicillin dose)")
+    plt.show()
+    print(spearmanr(delta_log_p, test_y))
+    
+    
+    
+
         
