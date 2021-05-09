a788dc56f47ec42ff5a69381efc1f71202adcd31
Richard Michael <rcml@novozymes.com>
2021-05-09 12:21:47 +0200

    fix test_y

--- parse_data.py
+++ parse_data.py
@@ -96,8 +96,6 @@ def parse_HEX():
     family_seqs = np.loadtxt("./data/hex/uniref90_MSA_.aln", dtype=str)
     family_seqs = np.array([np.array([seq2idx(s) for s in seq]).flatten() for seq in family_seqs])
     test_y = test_df["ddG (HIF)"]
-    # TODO test diff of test_seq[0] != WT and is == K309 mutation
-    # TODO test if encoding is equal
     return family_seqs, test_seqs, test_y
 
 
