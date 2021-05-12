correction of constant factor, corrected S_minmax scaling in testing

graphkernel.py
@@ -121,7 +121,7 @@ class VaeKernel:
         self.eigen_values = []
         if fixed_sample:
             self.latent_sample = torch.ones((sample_size, self.latent_dim)).float()
-        self.p_z = Normal(loc=torch.zeros(vae.z_dim), scale=torch.ones(vae.z_dim)).log_prob(self.latent_sample).sum(1)
+        self.p_z = Normal(loc=torch.zeros(vae.z_dim), scale=torch.ones(vae.z_dim)).log_prob(self.latent_sample).to(torch.float64).sum(1)
 
     def convert_one_hot(self, x):
         x = torch.Tensor(x).to(torch.int64)
@@ -134,9 +134,11 @@ class VaeKernel:
         AAs = 21
         N, L = sequences.shape
         s = torch.zeros([AAs])
+        p_x = Categorical(self.vae.decoder(self.compute_encoder_dist(sequences).loc).exp()).log_prob(torch.Tensor(sequences))
         # marginalize over residues
         for idx, _ in tqdm(enumerate(range(L))):
-            ll_S = np.exp(self.log_likelihood(sequences, idx))
+            # subtract normalizing constant
+            ll_S = np.exp(self.log_likelihood(sequences, idx).to(torch.float64) - p_x - sequences.shape[1]*np.log(20))
             # marginal - sum probs per AA across sequences
             s += ll_S.sum(0)[:AAs]
         s = np.log(s[:, np.newaxis] @ s[:, np.newaxis].T)
@@ -157,14 +159,14 @@ class VaeKernel:
         # compute x and y likelihoods
         z_x_loc, z_x_scale = self.vae.encoder(oh_x)
         z = z_x_loc + self.latent_sample[:, np.newaxis] * torch.sqrt(z_x_scale)
-        q_z_x = Normal(z_x_loc, z_x_scale).log_prob(z).sum(-1)
+        q_z_x = Normal(z_x_loc, z_x_scale).log_prob(z).to(torch.float64).sum(-1)
         p_z = torch.mean(self.p_z, axis=0)  # prior mean across samples
         # decoder can only evaluate one z at a time # TODO: refactor VAE to enable batched latent processing
         categoricals = [Categorical(self.vae.decoder(z_i).exp()) for z_i in z]
-        p_x_z_vec = torch.stack([cat.probs.log() for cat in categoricals])
-        p_x_z = torch.stack([cat.log_prob(torch.Tensor(x)) for cat in categoricals])
+        p_x_z_vec = torch.stack([cat.probs.log().to(torch.float64) for cat in categoricals])
+        p_x_z = torch.stack([cat.log_prob(torch.Tensor(x)).to(torch.float64) for cat in categoricals])
         p_x_not_i = self.p_x_not_i(p_x_z=p_x_z, p_z=p_z, q_z_x=q_z_x, c=c, i=i, L=L)
-        ll_x_i_x_not_i = torch.mean(p_x_z_vec[:, :, i] + p_x_not_i[:, :, np.newaxis] - (q_z_x/L)[:, :, np.newaxis] - (c/L) + (p_z/L),
+        ll_x_i_x_not_i = torch.mean(p_x_z_vec[:, :, i] + p_x_not_i[:, :, np.newaxis] - (q_z_x/L)[:, :, np.newaxis] + (c/L) + (p_z/L),
                                     axis=0)
         return ll_x_i_x_not_i
 
@@ -174,15 +176,15 @@ class VaeKernel:
         As defined in equation, sum over log-likelihoods
         => p(X_1=x_1, X_2=x_2, ... ,X_L=x_L) not inluding X_i
         """
-        p_x_not_i_lower_idx = torch.sum(p_x_z[:, :, :i] + (p_z/L) - (q_z_x/L)[:, :, np.newaxis] - (c/L), axis=-1)  # sum per sequence
-        p_x_not_i_higher_idx = torch.sum(p_x_z[:, :, (i+1):] + (p_z/L) - (q_z_x/L)[:, :, np.newaxis] - (c/L), axis=-1)
+        p_x_not_i_lower_idx = torch.sum(p_x_z[:, :, :i] + (p_z/L) - (q_z_x/L)[:, :, np.newaxis] + (c/L), axis=-1)  # sum per sequence
+        p_x_not_i_higher_idx = torch.sum(p_x_z[:, :, (i+1):] + (p_z/L) - (q_z_x/L)[:, :, np.newaxis] + (c/L), axis=-1)
         return p_x_not_i_lower_idx + p_x_not_i_higher_idx
 
     @torch.no_grad()
     def log_likelihood_idx(self, x: torch.Tensor, i: int):
         c = x.shape[1]*np.log(20)
         p_x = Categorical(self.vae.decoder(self.compute_encoder_dist(x).loc).exp()).log_prob(torch.Tensor(x))
-        ll = self.log_likelihood(x, i) - c
+        ll = self.log_likelihood(x, i).to(torch.float64) - c
         p_x_not_i = torch.log(torch.sum(torch.exp(ll), axis=-1))
         normalized_p_x_i_x_not_i = torch.diag(ll[:, x[:, i]]) - p_x[:, i] - p_x_not_i
         return normalized_p_x_i_x_not_i.detach().numpy()[:, np.newaxis]
@@ -210,7 +212,7 @@ class VaeKernel:
     def set_min_max_S(self, x_p, x_q) -> None:
         assert x_p.shape == x_q.shape
         print("Computing min, max S-values...")
-        for idx in range(x_p.shape[1]):
+        for idx in tqdm(range(x_p.shape[1])):
             s_mat = np.log(np.matmul(np.exp(self.k_vec(x_p, idx)), np.exp(self.k_vec(x_q, idx).T)))
             self.s_min = np.min(s_mat) if np.min(s_mat) <= self.s_min else self.s_min
             self.s_max = np.max(s_mat[s_mat!=np.inf]) if np.max(s_mat) >= self.s_max else self.s_max

parse_data.py
@@ -114,16 +114,16 @@ def parse_PGA():
 
 
 def parse_UBQ():
-    ubq_df = filter_alignment("./data/ubq/UBQ_combined_UBC_ISG15.a2m")
+    ubq_df = filter_alignment("/home/rimichael/pro_tooling/data/ubq/UBQ_combined_UBC_ISG15.a2m")
     family_seqs = np.array([[int(elem) for elem in seq] for seq in ubq_df.seq])
     # for testing combine protabank sequences with DeepSequence Bolon 2013 data
-    protabank_df = pd.read_csv("./data/ubq/RL401_Bolon2013_YHUnpqbw.csv", delimiter=",")
+    protabank_df = pd.read_csv("/home/rimichael/pro_tooling/data/ubq/RL401_Bolon2013_YHUnpqbw.csv", delimiter=",")
     # drop SD values
     protabank_df = protabank_df[~protabank_df["Assay/Protocol"].str.contains("SD ")]
     # drop last two elements from sequence "...GG" and duplicate last residues
     protabank_df["Sequence"] = protabank_df.Sequence
     # measurements as used in DeepSequence paper
-    deep_seq_df = pd.read_csv("./data/ubq/RL401_Bolon2013.csv", delimiter=";")
+    deep_seq_df = pd.read_csv("/home/rimichael/pro_tooling/data/ubq/RL401_Bolon2013.csv", delimiter=";")
     deep_seq_df = deep_seq_df[["mutant", "selection_coefficient"]].dropna()
     test_df = deep_seq_df.merge(protabank_df[["Description", "Data", "Sequence"]],
                                 "inner", left_on="mutant", right_on="Description")

run_kernel_experiment.py
@@ -5,21 +5,16 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 import os
-import logging
-import warnings
 import argparse
 import mlflow
-import seaborn as sns
-import matplotlib.pyplot as plt
-import random
-from utility import parse_BLAT, parse_UBQ, parse_PGA, parse_TLL
-from reference_alphabet import seq2idx
+from parse_data import parse_BLAT, parse_UBQ, parse_PGA, parse_TLL
 from visualization import plot_SVAE_matrix, plot_VAE_kernel_values, plot_eigenvalues
 
 # DATA_DICT = {"blat": "2lzm", "sp400": "", "ubq": "1ubq", 
 #             "pga": "1pga", "hexo": ""}
-DATA_DICT = {"blat": "2lzm",  # "ubq": "1ubq",
-             # "pga": "1pga",
+DATA_DICT = {"blat": "2lzm",
+             "ubq": "1ubq",
+             "pga": "1pga",
              }
 VAE_TYPES = list(DATA_DICT.keys())
 PDB_FILES = list(DATA_DICT.values())
@@ -28,32 +23,29 @@ VAE_PARAMETERS = {"LATENT_DIM": 55, "ENCODER_DIM": [1700],
                   "DROPOUT": 0.065}
 
 
-def run_and_plot_S_matrix(vae_type, vae, sequences, marginal, run_suffix=""):
+def run_and_plot_S_matrix(vae_type, vae, sequences, run_suffix=""):
     """
     Compute S Matrix from 1000 sequences
     """
-    s_k = VaeKernel(vae, marginal_not_i=marginal)
-    S_mat_fam = s_k.compute_S_matrix(sequences[:1000, :], normalize=False)
+    s_k = VaeKernel(vae)
+    # S_mat_fam = s_k.compute_S_matrix(sequences[:1000, :], normalize=False)
+    # plot_SVAE_matrix(S_mat_fam, name_suffix=vae_type.upper(), run_suffix=run_suffix+" not normalized")
     S_mat_fam_norm = s_k.compute_S_matrix(sequences[:1000, :], normalize=True)
-    plot_SVAE_matrix(S_mat_fam, name_suffix=vae_type.upper(), run_suffix=run_suffix+" not normalized")
     plot_SVAE_matrix(S_mat_fam_norm, name_suffix=vae_type.upper(), run_suffix=run_suffix+" normalized")
 
 
 def run_and_plot_kernel(vae_type, vae, family_seqs, test_seqs, adjacencies,
-                        normalize_S, normalize_k, marginal_not_i, subset_n=None):
+                        normalize_S, normalize_k, subset_n=None):
     family_seqs = family_seqs[:subset_n, :] if subset_n else family_seqs
     test_seqs = test_seqs[:subset_n, :] if subset_n else test_seqs
 
-    v_k = VaeKernel(vae, normalize_S=normalize_S, marginal_not_i=marginal_not_i,
-                    eigen=True)
-    s_vae_val_fam = v_k.k(family_seqs, adjacencies=adjacencies,
-                          normalize_k=normalize_k)
+    v_k = VaeKernel(vae, normalize_S=normalize_S, eigen=True)
+    s_vae_val_fam = v_k.k(family_seqs, adjacencies=adjacencies, normalize_k=normalize_k)
     s_fam_eigen = v_k.eigen_values
     v_k.eigen_values = []
     fam_eigen_vec_real = np.stack([eig.real for eig in s_fam_eigen]).flatten()
     fam_eigen_vec_imag = np.stack([eig.imag for eig in s_fam_eigen]).flatten()
-    s_vae_val_test = v_k.k(test_seqs, adjacencies=adjacencies,
-                           normalize_k=normalize_k)
+    s_vae_val_test = v_k.k(test_seqs, adjacencies=adjacencies, normalize_k=normalize_k)
     s_test_eigen = v_k.eigen_values
     v_k.eigen_values = []
     test_eigen_vec_real = np.stack([eig.real for eig in s_test_eigen]).flatten()
@@ -69,7 +61,7 @@ def run_and_plot_kernel(vae_type, vae, family_seqs, test_seqs, adjacencies,
 
 @torch.no_grad()
 def experiment_routine(vae_type, family_seqs, test_seqs):
-    num_classes = np.unique(family_seqs).shape[0]  #+2 # for UBQ and PGA
+    num_classes = np.unique(family_seqs).shape[0] + 1
     WT = F.one_hot(torch.tensor(family_seqs[0], dtype=torch.int64), num_classes=num_classes).flatten().float()
     # init VAE
     input_dim = WT.shape[0]
@@ -89,14 +81,8 @@ def experiment_routine(vae_type, family_seqs, test_seqs):
     vae.eval()
     pdb_name = DATA_DICT.get(vae_type)
     # DERIVE S MATRIX
-    # run_and_plot_S_matrix(vae_type=vae_type, vae=vae, sequences=family_seqs,
-    #                       marginal=False, run_suffix="joint ")
-    # run_and_plot_S_matrix(vae_type=vae_type, vae=vae, sequences=test_seqs,
-    #                       marginal=False, run_suffix="joint ")
-    # run_and_plot_S_matrix(vae_type=vae_type, vae=vae, sequences=family_seqs,
-    #                       marginal=True, run_suffix="marginal ")
-    # run_and_plot_S_matrix(vae_type=vae_type, vae=vae, sequences=test_seqs,
-    #                       marginal=True, run_suffix="marginal ")
+    run_and_plot_S_matrix(vae_type=vae_type, vae=vae, sequences=family_seqs, run_suffix="MSA ")
+    run_and_plot_S_matrix(vae_type=vae_type, vae=vae, sequences=test_seqs, run_suffix="SSL ")
     # TEST KERNEL
     contact_map = ContactMapper(pdb_file=f"./pdb/{pdb_name}.pdb", tri_dist=True)
     ref_adj = contact_map.adjacency
@@ -125,7 +111,7 @@ def experiment_routine(vae_type, family_seqs, test_seqs):
     # #                     normalize_S=False, normalize_k=True, marginal_not_i=True)
     run_and_plot_kernel(vae_type=vae_type, vae=vae, family_seqs=family_seqs,
                         test_seqs=test_seqs, adjacencies=ref_adj,
-                        normalize_S=True, normalize_k=True, marginal_not_i=True, subset_n=100)
+                        normalize_S=True, normalize_k=True, subset_n=100)
     # select subset for plotting DIFFs
     # run_and_plot_kernel(vae_type=vae_type, vae=vae, family_seqs=family_seqs,
     #                     test_seqs=test_seqs, adjacencies=ref_adj,
@@ -142,8 +128,6 @@ if __name__ == "__main__":
     parser.add_argument("--experiment", type=str, help="experiment str as ID for tracking.")
     parser.add_argument("--vae_type", nargs="+", type=str, default=VAE_TYPES,
                         help="List of identifiers of VAEs for which to run kernel experiments.")
-    parser.add_argument("-p", "--plot", action="store_false",
-                        help="Plot low-latent-representation outputs and feature correlation.")
     args = parser.parse_args()
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)

test/test_vae_kernel.py
@@ -118,7 +118,7 @@ def S_stable(_vae, seq_x, seq_y, idx: int, n_samples=1, fixed_sample=False):
     latent_samples = torch.normal(0, 1, size=(n_samples, _vae.z_dim)).float()
     if fixed_sample:
         latent_samples = torch.ones((n_samples, _vae.z_dim)).float()
-    c = np.log(20**L)
+    c = L*np.log(20)
     oh_x = F.one_hot(torch.Tensor(seq_x).to(torch.int64), num_classes=_vae.num_categories).float()
     oh_y = F.one_hot(torch.Tensor(seq_y).to(torch.int64), num_classes=_vae.num_categories).float()
     z_x_dist = _vae.encoder(oh_x)
@@ -136,8 +136,8 @@ def S_stable(_vae, seq_x, seq_y, idx: int, n_samples=1, fixed_sample=False):
     ll_x_i_x_not_i_vec = torch.stack(ll_x_i_x_not_i_vec) - c
     ll_y_i_y_not_i_vec = torch.stack(ll_y_i_y_not_i_vec) - c
     # SUM OF PROBABILITIES - hence exp operation
-    ll_x_not_i = torch.log(torch.sum(torch.exp(torch.mean(ll_x_i_x_not_i_vec, axis=0))))
-    ll_y_not_i = torch.log(torch.sum(torch.exp(torch.mean(ll_y_i_y_not_i_vec, axis=0))))
+    ll_x_not_i = torch.log(torch.sum(torch.exp(torch.mean(ll_x_i_x_not_i_vec, axis=0).to(torch.float64))))
+    ll_y_not_i = torch.log(torch.sum(torch.exp(torch.mean(ll_y_i_y_not_i_vec, axis=0).to(torch.float64))))
     normalized_ll_x_i_x_not_i = torch.mean(ll_x_i_x_not_i_vec, axis=0)[seq_x[idx]] - p_x[:, idx] - ll_x_not_i
     normalized_ll_y_i_y_not_i = torch.mean(ll_y_i_y_not_i_vec, axis=0)[seq_y[idx]] - p_y[:, idx] - ll_y_not_i
     return normalized_ll_x_i_x_not_i + normalized_ll_y_i_y_not_i
@@ -178,7 +178,7 @@ def get_min_max_S_vals(sequences, vae, sample_size=1, fixed_sample=False):
     for p in range(n):
         for q in range(n):
             for idx in range(sequences.shape[1]):
-                s_val = S(vae, seq_x=sequences[p], seq_y=sequences[q], idx=idx, n_samples=sample_size,
+                s_val = S_stable(vae, seq_x=sequences[p], seq_y=sequences[q], idx=idx, n_samples=sample_size,
                           fixed_sample=fixed_sample)
                 s_min = np.float(s_val) if s_val <= s_min else s_min
                 s_max = np.float(s_val) if s_val >= s_max else s_max
@@ -274,13 +274,8 @@ def test_vectorized_VAE_kernel():
     v_k = VaeKernel(vae, sample_size=10, fixed_sample=True, normalize_S=False)
     s_vae_val = v_k.k(sequences, adjacencies=adj, normalize_k=False)
     print(s_vae_val)
-    norm = np.sqrt(np.diag(s_vae_val))
-    norm_s_vae_val = s_vae_val / norm.dot(norm.T)
     # TEST UNNORMALIZED
     np.testing.assert_almost_equal(naive_vae_val, s_vae_val, decimal=5)
-    # normalized_naive_vae_val = normalize_K(naive_vae_val)
-    # # TEST NORMALIZED
-    # np.testing.assert_almost_equal(normalized_naive_vae_val, norm_s_vae_val)
 
 
 def test_vectorized_normalized_VAE_kernel():
@@ -292,7 +287,7 @@ def test_vectorized_normalized_VAE_kernel():
     norm = np.sqrt(np.diag(s_vae_val))
     norm_s_vae_val = s_vae_val / norm.dot(norm.T)
     # TEST NOT NORMALIZED
-    np.testing.assert_almost_equal(naive_vae_val, s_vae_val, decimal=4)
+    np.testing.assert_almost_equal(naive_vae_val, s_vae_val, decimal=5)
     # normalized_naive_vae_val = normalize_K(naive_vae_val)
     # # TEST NORMALIZED
     # np.testing.assert_almost_equal(normalized_naive_vae_val, norm_s_vae_val)
@@ -303,13 +298,25 @@ def test_vectorized_VAE_kernel_on_UBQ():
     contact_map = ContactMapper(pdb_file=f"/home/rimichael/pro_tooling/pdb/1ubq.pdb", tri_dist=True)
     sequences = family_seqs[:3]
     ref_adj = [c for elem, c in contact_map.adjacency]
-    naive_vae_val = naive_v_K(sequences, ref_adj, vae, sample_size=10, stable=True, fixed_sample=True)
-    v_k = VaeKernel(vae, sample_size=10, fixed_sample=True)
-    s_vae_val = v_k.k(sequences, adjacencies=ref_adj, normalize=False)
+    naive_vae_val = naive_v_K(sequences, ref_adj, vae, sample_size=10, stable=True, fixed_sample=True, normalize_S=False)
+    v_k = VaeKernel(vae, sample_size=10, fixed_sample=True, normalize_S=False)
+    s_vae_val = v_k.k(sequences, adjacencies=ref_adj, normalize_k=False)
     norm = np.sqrt(np.diag(s_vae_val))
     norm_s_vae_val = s_vae_val / norm.dot(norm.T)
     # TEST NOT NORMALIZED
-    # np.testing.assert_almost_equal(naive_vae_val, s_vae_val)
-    # TEST NORMALIZED
-    normalized_naive_vae_val = normalize_K(naive_vae_val)
-    np.testing.assert_almost_equal(normalized_naive_vae_val, norm_s_vae_val)
+    np.testing.assert_almost_equal(naive_vae_val, s_vae_val, decimal=4)
+    # # TEST NORMALIZED
+    # normalized_naive_vae_val = normalize_K(naive_vae_val)
+    # np.testing.assert_almost_equal(normalized_naive_vae_val, norm_s_vae_val)
+
+
+def test_vectorized_VAE_kernel_normalized_on_UBQ():
+    family_seqs, vae = setup_UBQ_VAE()
+    contact_map = ContactMapper(pdb_file=f"/home/rimichael/pro_tooling/pdb/1ubq.pdb", tri_dist=True)
+    sequences = family_seqs[:2]
+    ref_adj = [c for elem, c in contact_map.adjacency]
+    naive_vae_val = naive_v_K(sequences, ref_adj, vae, sample_size=5, stable=True, fixed_sample=True, normalize_S=True)
+    v_k = VaeKernel(vae, sample_size=5, fixed_sample=True, normalize_S=True)
+    s_vae_val = v_k.k(sequences, adjacencies=ref_adj, normalize_k=False)
+    # TEST NOT NORMALIZED
+    np.testing.assert_almost_equal(naive_vae_val, s_vae_val, decimal=4)